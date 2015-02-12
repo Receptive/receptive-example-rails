0c4da2bf6c6f2c3748504831354b1ab1867dfb01
Dan Dukeson <dandukeson@gmail.com>
2015-02-12 17:41:28 +0000

    move keys out to a config initializer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,27 +9,23 @@ class HomeController < ApplicationController
         "monthly_value" => "99.99"
       },
       "vendor" => {
-        "id" => "VENDOR ID GOES HERE" # put your own vendor ID here
+        "id" => Rails.configuration.receptive_vendor_id
       },
       "user" => {
         "full_name" => "Robo Cop",
         "email" => "john@example.com",
         "id" => "u123456"
       },
-      "return_url" => "http://example.com/user/u123456",
+      "return_url" => "http://localhost:3001",
     }    
     @signed_snippet = sign_snippet(unsigned_snippet)
   end
   
   private
 
-  # don't expose this to your app users
-  # get your own secret from https://receptive.io/app/#/integrate
-  #
-  SECRET = "secret goes here" 
 
   def sign_snippet(unsigned)
-    concat_values = ([SECRET] + [unsigned["return_url"]] + [unsigned["timestamp"]] + unsigned["account"].values + unsigned["user"].values + unsigned["vendor"].values).sort.join(",")
+    concat_values = ([Rails.configuration.receptive_secret_key] + [unsigned["return_url"]] + [unsigned["timestamp"]] + unsigned["account"].values + unsigned["user"].values + unsigned["vendor"].values).sort.join(",")
     signature = Digest::SHA256.new << concat_values
     unsigned["signature"] = signature.to_s
     unsigned.to_json
---
+++ config/initializers/receptive.rb
@@ -0,0 +1,9 @@
+#
+# API keys etc for Receptive integration.
+# Look here for your values: https://receptive.io/app/#/integrate
+#
+
+Rails.configuration.receptive_secret_key="CHANGEME"
+Rails.configuration.receptive_vendor_id="CHANGEME"
+
+# restart rails after changing these values
